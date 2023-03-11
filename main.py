bf5cf93e624d2fe0b3dcb2fa38f4efab42e3d010
zorcanbarsk <zorcan44@gmail.com>
2023-03-11 15:16:23 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,110 @@
+print("Kodlamaio")
+baslik = "Taşit kredisi"
+print(baslik)
+# string = 
+baslik = "selam"
+print(baslik)
+#int. integer
+vade = 36
+ekVade = 6
+vade2 = "36"
+
+# float decimal double
+aylikOdeme = 200.50
+
+# bool boolean = true yada false 
+yukselisteMi = False
+
+# matamatiksel operatörler
+
+# + 
+print(5+5)
+print(vade + 12)
+print(ekVade + vade)
+print(36+6)
+
+#-
+print(5-5)
+print(vade-12)
+print(vade- ekVade)
+print(36-6)
+
+# *
+print(5*5)
+print(vade*2)
+print(vade*ekVade)
+print(10*10)
+
+# /
+print(5/5)
+print(vade / 2)
+print(vade / ekVade)
+print(10/2)
+
+
+yeniVade = vade / 2 
+fiyat = 100
+İndirimliFiyati = fiyat - 20 
+
+print(yeniVade)
+print(İndirimliFiyati)
+
+# mod operatör
+print(10%2)
+print(vade%5)
+print(vade % ekVade)
+print(30%10)
+
+
+# mantıksal operatör
+print(1==1)
+print(1==2)
+
+print(1<2)
+
+print(1>1)
+
+print(1<=2)
+
+print(1>=1)
+
+print(1 != 1)
+
+print(1 != 2)
+
+
+
+
+#  or and
+
+
+print(1>2 or 5>2)
+print(1>2 and 5>2)
+print(1>2 or 5>2 and 3>2)
+
+print(1>2 and 5>2 and 3>2)
+
+print(2>1 or 1>2 and 1>2)
+
+# karar yapilari
+# if else 
+
+
+sayi1 = 15
+sayi2 = 15
+
+# sayi1 sayi 2 den buyukse ekrana sayi 1 daha buyuk yazdir
+
+if sayi1 <= sayi2:
+    print("sayi 1 sayi 2 den KUCUKTUR ve esittir knk")
+# eger if bloguna girmez ise
+elif sayi1 == sayi2:
+    print("2 sayi esittirr")
+# if ve elif durumu da olmazsa 
+else:
+    print("sAYI 1 SAYI 2 DEN BUYUKTUR")
+
+print("burasi if blogunun disi")
+
+
+
